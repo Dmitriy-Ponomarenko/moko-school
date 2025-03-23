273c631c79d421e59e4d28897d062943a19848d5
Dmitriy-Ponomarenko <dponomar19@gmail.com>
2025-03-23 09:38:47 +0100

    hero-section mobile

--- src/js/index.js
+++ src/js/index.js
@@ -2,10 +2,12 @@
 
 export function initializeApp() {
   // Здесь инициализация всех функций для приложения, в том числе для бургер-меню
-  import("./partials/burger-menu.js").then((module) => {
-    // Код после загрузки burger-menu.js
-    console.log('Бургер-меню загружено и готово!');
-  }).catch((error) => {
-    console.error('Ошибка при загрузке бургер-меню:', error);
-  });
+  import('./partials/burger-menu.js')
+    .then(module => {
+      // Код после загрузки burger-menu.js
+      console.log('Бургер-меню загружено и готово!');
+    })
+    .catch(error => {
+      console.error('Ошибка при загрузке бургер-меню:', error);
+    });
 }
--- src/js/partials/burger-menu.js
+++ src/js/partials/burger-menu.js
@@ -1,23 +1,23 @@
 // src/js/partionals/burger-menu.js
 
-document.addEventListener("DOMContentLoaded", () => {
-  const burgerButton = document.getElementById("burger-button");
-  const closeButton = document.getElementById("close-menu");
-  const mobileMenu = document.getElementById("mobile-menu");
+document.addEventListener('DOMContentLoaded', () => {
+  const burgerButton = document.getElementById('burger-button');
+  const closeButton = document.getElementById('close-menu');
+  const mobileMenu = document.getElementById('mobile-menu');
 
   if (burgerButton && closeButton && mobileMenu) {
-    burgerButton.addEventListener("click", () => {
-      mobileMenu.classList.add("open");
+    burgerButton.addEventListener('click', () => {
+      mobileMenu.classList.add('open');
     });
 
-    closeButton.addEventListener("click", () => {
-      mobileMenu.classList.remove("open");
+    closeButton.addEventListener('click', () => {
+      mobileMenu.classList.remove('open');
     });
 
     // Закрытие при клике вне меню
-    mobileMenu.addEventListener("click", (event) => {
+    mobileMenu.addEventListener('click', event => {
       if (event.target === mobileMenu) {
-        mobileMenu.classList.remove("open");
+        mobileMenu.classList.remove('open');
       }
     });
   }
